98cf892ea38fb66d68bf826c5e1ae7faa00f3fe9
Andrew Barna <abarna@gmail.com>
2018-08-24 12:49:04 -0700

    add more files and a progress indicator

--- src/App.js
+++ src/App.js
@@ -31,10 +31,22 @@ class View extends Component {
 }
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      progress: 0
+    }
+  }
+
   download() {
     let data = [
       "https://cchdo.ucsd.edu/data/12130/318M20091121_hy1.csv",
-      "https://cchdo.ucsd.edu/data/12131/318M20091121_nc_hyd.zip"
+      "https://cchdo.ucsd.edu/data/12131/318M20091121_nc_hyd.zip",
+      "https://cchdo.ucsd.edu/data/12132/318M20091121hy.txt",
+      "https://cchdo.ucsd.edu/data/13792/318M20091121_ct1.zip",
+      "https://cchdo.ucsd.edu/data/13793/318M20091121_nc_ctd.zip",
+      "https://cchdo.ucsd.edu/data/3102/p06_318M20091121do.txt",
+      "https://cchdo.ucsd.edu/data/7024/p06_318M20091121su.txt",
     ];
     let zipfile = new JSZip();
 
@@ -43,12 +55,14 @@ class App extends Component {
     data.map((path) => {
       zipfile.file("data/" + path.split("/")[5], fetch(path).then((res) => res.blob()));
     })
-    zipfile.generateAsync({type:"blob"}).then((blob) => saveAs(blob, "p6_bottle_data_part1.zip"))
+    zipfile.generateAsync({type:"blob"}, (meta) => this.setState({progress:meta.percent})).then((blob) => saveAs(blob, "p6_bottle_data_part1.zip"))
   }
   render() {
     return (
       <div className="App">
         <button onClick={() => this.download()}>Download some data!</button>
+        <br />
+        <progress max="100" value={this.state.progress}></progress>
         <View />
       </div>
     );
